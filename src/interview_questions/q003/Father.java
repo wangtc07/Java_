1d67452b6971308287eb9ef1f19807a230b7c508
wangtc07 <wangtc07@gmail.com>
2022-05-26 23:48:50 +0800

    class init

--- src/interview_questions/q003/Father.java
+++ src/interview_questions/q003/Father.java
@@ -1,2 +1,28 @@
-package interview_questions.q003;public class Father {
+package interview_questions.q003;
+
+public class Father {
+  private int i = test();
+  private static int j = method();
+
+  static {
+    System.out.print(1 + " - ");
+  }
+
+  public Father() {
+    System.out.print(2 + " - ");
+  }
+
+  {
+    System.out.print(3 + " - ");
+  }
+
+  public int test() {
+    System.out.print(4 + " - ");
+    return 1;
+  }
+
+  private static int method() {
+    System.out.print(5 + " - ");
+    return 1;
+  }
 }
