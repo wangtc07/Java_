6b77e60d1c65cb50b0fb14109ebde7b0a9081dca
wangtc07 <wangtc07@gmail.com>
2022-06-03 09:15:52 +0800

    Q004

--- src/interview_questions/q001/Q001.java
+++ src/interview_questions/q001/Q001.java
@@ -5,11 +5,10 @@ public class Q001 {
   public static void main(String[] args) {
     int i = 1;
     i = i++; // 1
-    System.out.println(i);
-    int j = i++; // 2
-    System.out.println(j);
-    int k = i + ++i * i++; // 2 + 3 * 3 = 11
-    System.out.println(k);
-
+    System.out.println(i); // 1
+    int j = i++; // i == 2
+    System.out.println(j); // 1
+    int k = i + ++i * i++; // 2 + 3 * 3
+    System.out.println(k); // 11
   }
 }
