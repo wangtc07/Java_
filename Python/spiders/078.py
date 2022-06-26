5853dd7d902f34ac94310c0ea840a5cc5420276e
Wangtc07 <wangtc07@gmail.com>
2022-06-27 00:23:41 +0800

    python 078

--- Python/spiders/078.py
+++ Python/spiders/078.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-url = 'https://tw.jd.com/'
+# url = 'https://tw.jd.com/'
+url = 'http://dcimg.awalker.jp/i/6hFRpVkeeZQGol0FirjaMGGxhCstxFOp0wdkCcM0vl5xX7wSp3RlyZjeBvhHPRUh6SG5bdMFTrEADDS0v16Y4LmrSOFlxnFviPuOXV5KKGOzqC0g282YZlCdo6SDC1llWPTQhM1m2sYyGiqnORA8m2HcT1Sx4vVfYpUKb1Cy2DxMONs1EZ1vP2ahj6JJ3heSyK2WbyEo'
+
 # url = 'https://www.youtube.com/'
 
 path = './chromedriver'
