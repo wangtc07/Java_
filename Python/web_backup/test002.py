cb6204a9355f294768bd8be87df98610f2e84d54
Wangtc07 <wangtc07@gmail.com>
2022-06-28 00:45:55 +0800

    python web_backup
    cookies

---
+++ Python/web_backup/test002.py
@@ -0,0 +1,168 @@
+import re
+import time
+import requests
+import threading
+from lxml import etree
+from bs4 import BeautifulSoup
+from queue import Queue
+from threading import Thread
+import utils
+
+
+def run(index, wait_html_urls, out_downloaded_urls: Queue,
+    out_other_urls: Queue):
+  print(index, '----------')
+  test = 1
+  while wait_html_urls.empty() is not True:
+    # if test > 2:
+    #   wait_html_urls = Queue()
+    #   break
+    test = test + 1
+    url = wait_html_urls.get()
+    print('download: ', url)
+    # 移除 ima
+    n_url = utils.re_link_path(url, 0)
+
+    # 檢查是否下載過
+    # ./s/n46/diary/MEMBER/list?&page=0&ct=36753&cd=MEMBER.html
+    html_url = utils.add_html(n_url)
+    content = html_url in out_downloaded_urls.queue
+    print('檢查是否下載過: ', content)
+
+    # 下載過 -> 跳過
+    if content:
+      continue
+
+    # 下載 html, 返回未下載連結
+    urls = utils.download(url)
+
+    # 區分 .html 和 其他
+    all_urls = utils.dis_urls(urls)
+    html_urls = all_urls.html
+    # 沒下載過加回待下載 html
+    for html_url in html_urls:
+      content = html_url in out_downloaded_urls.queue
+      if content:
+        continue
+      # else:
+      # wait_html_urls.put(html_url)
+
+    # 其他 待下載 url
+    oth_urls: list = all_urls.oth
+    out_oth_urls = set(out_other_urls.queue)
+
+    # 移除下載過的
+    for other_url in oth_urls:
+      out_oth_urls.add(other_url)
+    for other_url in out_oth_urls:
+      out_other_urls.put(other_url)
+
+    ed_url = utils.reaplce_reg_text(url)
+    out_downloaded_urls.put(ed_url)
+    print('download done ----------', ed_url)
+    wait_html_urls.task_done()
+
+  # if html.empty() is not True:
+  #   run(index - 1, html, out_downloaded_urls)
+
+
+def download_oth(out_other_urls: Queue, error_urls: Queue, ed_urls: Queue):
+  while out_other_urls.empty() is not True:
+    url = out_other_urls.get()
+
+    if url in ed_urls.queue:
+      out_other_urls.task_done()
+      continue
+
+    # dc img
+    if utils.is_dc_page(url):
+      path = utils.get_dc_path(url)
+      try:
+        utils.download_dcimg(path, url)
+      except:
+        error_urls.put(url)
+    # 其他資源
+    else:
+      utils.download_path(url)
+
+    out_other_urls.task_done()
+
+
+download_log = 'downloaded.log'
+
+if __name__ == '__main__':
+  start = time.time()
+  # 要下載的 html url
+  queue = Queue()
+  # 下載完的 url
+  downloaded_queue = Queue()
+  # 要下載的 other url
+  other_urls = Queue()
+  # 下載錯誤 url
+  error_urls = Queue()
+
+  downloaded = open(download_log, encoding='UTF-8')
+  text_list = downloaded.read().split('\n')
+  for url in text_list:
+    downloaded_queue.put(url)
+
+  # 測試 list
+  for i in range(0, 10):
+    queue.put(
+        '/s/n46/diary/MEMBER/list?ima=3233&page=' + str(
+            i) + '&ct=36753&cd=MEMBER')
+
+
+
+  # 測試 blog
+  # queue.put('https://www.nogizaka46.com/s/n46/diary/detail/56454?ima=3811&cd=MEMBER')
+
+  # print('--------------------------------------------')
+  # while queue.empty() is not True:
+  #   ed = queue.get()
+  #   print(ed)
+
+  # run(1, queue, downloaded_queue, other_urls)
+  #
+  # print('--------------------------------------------')
+  # while downloaded_queue.empty() is not True:
+  #   ed = downloaded_queue.get()
+  #   print(ed)
+  #
+  # print('--------------------------------------------')
+  # while other_urls.empty() is not True:
+  #   oth = other_urls.get()
+  #   print(oth)
+
+  print('queue 開始大小 %d' % queue.qsize())
+
+  for index in range(10):
+    thread = Thread(target=run,
+                    args=(index, queue, downloaded_queue, other_urls,))
+
+  thread.daemon = True  # 隨主線程退出而退出
+  thread.start()
+
+  queue.join()  # 隊列消費完 線程結束
+
+  out = list(other_urls.queue)
+  log = '\n'.join(out)
+  open('wait_other.log', 'w').write(log)
+
+  print('--------------------------------------------')
+  print(queue.qsize())
+
+  print('other 開始大小 %d' % other_urls.qsize())
+
+  for i in range(0, 2):
+    thread2 = Thread(target=download_oth,
+                     args=(other_urls, error_urls, downloaded_queue))
+
+  thread2.daemon = True  # 隨主線程退出而退出
+  thread2.start()
+
+  other_urls.join()  # 隊列消費完 線程結束
+  print('--------------------------------------------')
+  print(other_urls.qsize())
+
+  # 下載 other_urls
